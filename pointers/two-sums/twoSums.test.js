6bcbc7e719f330c952549a786cd65792d8eed797
JasonEb <jasonebueng@gmail.com>
2020-10-09 19:37:26 -0700

    introduces two sums tests

--- pointers/two-sums/twoSums.test.js
+++ pointers/two-sums/twoSums.test.js
@@ -1,5 +1,19 @@
 const twoSum = require('./twoSums');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(twoSum(1, 2)).toBe(3);
+test('passes example 1', () => {
+  let numbers = [2,7,11,15]
+  let target = 9
+  expect(twoSum(numbers, target)).toBe([1,2]);
+});
+
+test('passes example 2', () => {
+  let numbers = [2,3,4]
+  let target = 6
+  expect(twoSum(numbers, target)).toBe([1,3]);
+});
+
+test('passes example 3', () => {
+  let numbers = [-1,0]
+  let target = -1
+  expect(twoSum(numbers, target)).toBe([1,2]);
 });
