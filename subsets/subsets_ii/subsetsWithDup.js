627ab561af0d4059a667e3a5509d9d84714a540b
JasonEb <jasonebueng@gmail.com>
2020-12-21 19:44:42 -0800

    adds original subsets approach

--- subsets/subsets_ii/subsetsWithDup.js
+++ subsets/subsets_ii/subsetsWithDup.js
@@ -1,4 +1,24 @@
-const subsetsWithDup = () => {
-    return []
+const subsetsWithDup = (nums) => {
+    let sol = []
+    let candidate = []
+    nums = nums.sort( (a,b) => { return a < b })
+    traverse(sol, candidate, nums)
+    return sol
+}
+
+const traverse = (sol, candidate, nums, idx = 0) => {
+    sol.push(candidate)
+    if (idx == nums.length) {
+        return 
+    }
+
+    for (let i = idx; i < nums.length; ++i) {
+        if(i != idx && nums[i] == nums[i - 1]){
+            //what?
+        }
+        candidates.push(nums[i])
+        traverse(sol, candidates, nums, i + 1)
+        candidates.pop()
+    }
 }
 exports.subsetsWithDup = subsetsWithDup
