178a2e0325f2f0b3bd3b8a66156262291bb4cc93
JasonEb <jasonebueng@gmail.com>
2020-12-22 19:30:27 -0800

    implements basic map

--- data_structures/basic-adts/map.test.js
+++ data_structures/basic-adts/map.test.js
@@ -6,9 +6,19 @@ describe('Map', ()=> {
     expect(map).toBeTruthy()
 
     it('sets keys', () => {
-        expect(map.set("a", "1")).toEqual(True)
-        expect(map.set("b", "2")).toEqual(True)
-        expect(map.set("ab", "3")).toEqual(True)
+        expect(map.set("a", "1")).toEqual(true)
+        expect(map.set("b", "2")).toEqual(true)
+        expect(map.set("ab", "3")).toEqual(true)
+
+        //ToDo: check for uniqueness
+    })
+
+    it('finds keys', () => {
+        expect(map.findKey("a")).toEqual(["a", "1"])
+        expect(map.findKey("b")).toEqual(["b","2"])
+        expect(map.findKey("ab")).toEqual(["ab","3"])
+        expect(map.findKey("abc")).toEqual(null)
+        //ToDo: check for uniqueness
     })
 
     it('gets keys', () => {
@@ -18,7 +28,7 @@ describe('Map', ()=> {
     })
 
     it('deletes keys', () => {
-        expect(map.delete("a")).toEqual(True)
+        expect(map.deleteKey("a")).toEqual(true)
         expect(map.get("a")).toEqual(null)
     })
 })
