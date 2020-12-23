093fa58bcd37ee7468bda4b708c43a09ecfe0cfb
JasonEb <jasonebueng@gmail.com>
2020-12-22 18:56:34 -0800

    implements queue

--- data_structures/basic-adts/queue.test.js
+++ data_structures/basic-adts/queue.test.js
@@ -1,7 +1,20 @@
 const Queue = require('./queue')
 
 describe('Queue', () =>{
-    let queue = Queue.new()
+    let queue = new Queue()
 
     expect(queue).toBeTruthy()
+
+    test('it enqueues', ()=>{
+        queue.enqueue(1)
+        expect(queue.peek()).toEqual(1)
+        queue.enqueue(10)
+        expect(queue.peek()).toEqual(10)
+    })
+
+    test('it dequeues', ()=>{
+        queue.dequeue()
+        queue.enqueue(2)
+        expect(queue.peek()).toEqual(2)
+    })
 })
