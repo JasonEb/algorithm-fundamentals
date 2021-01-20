54dcd9ebc31f85724f704b151871486e64533b12
JasonEb <jasonebueng@gmail.com>
2021-01-20 13:18:50 -0800

    adds naive solution

--- arrays/sumEvenAfterQueries/sumEvenAfterQueries.js
+++ arrays/sumEvenAfterQueries/sumEvenAfterQueries.js
@@ -1,5 +1,17 @@
 const sumEvenAfterQueries = (A, queries) => {
-    return A
+    let result = []
+    let idx = 0
+    let sum = 0
+    let evens = []
+    
+    queries.forEach( (query) => {
+        idx = query[1]
+        A[idx] += query[0]
+        evens = A.filter( (el) => el % 2 == 0 )
+        if(evens.length > 0 ){ sum = evens.reduce( (sum, el) => sum += el ) }
+        result.push(sum)
+    })
+    return result
 }
 
 exports.sumEvenAfterQueries = sumEvenAfterQueries
