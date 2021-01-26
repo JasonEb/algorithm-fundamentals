5a2ca3816c11cf87c6a8d202924b667b0005ac93
JasonEb <jasonebueng@gmail.com>
2021-01-25 21:29:58 -0800

    implements solution for weird_faculty

--- arrays/weird_faculty/solution.js
+++ arrays/weird_faculty/solution.js
@@ -1,5 +1,30 @@
-const solution = (input) => {
-    return input
+const solution = (answers) => {
+    n = answers.length
+
+    //convert 0's to -1
+    answers.forEach ( (el,idx) => {
+        if (el == 0){
+            answers[idx] = -1
+        }
+    })
+
+    let totalSum = answers.reduce( (sum, el) => sum += el )
+    let currSum = 0
+
+    //iterate through array to find currSum
+    //if the currSum is greater than the total sum
+    //return index
+
+    for(let i = 0; i < answers.length; i++) {
+        if (currSum > totalSum) {
+            return i
+        }
+    
+        currSum += answers[i]
+        totalSum -= answers[i]
+    }
+
+    return answers.length
 }
 
 exports.solution = solution
