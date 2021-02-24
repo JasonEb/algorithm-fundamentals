836e7282efcad2fe9c535aa12eb5bf0c41b2d152
JasonEb <jasonebueng@gmail.com>
2021-02-23 18:44:09 -0800

    solves max consecutive ones

--- arrays/max_consecutive_ones/findMaxConsecutiveOnes.js
+++ arrays/max_consecutive_ones/findMaxConsecutiveOnes.js
@@ -1,5 +1,21 @@
 const findMaxConsecutiveOnes = (arr) => {
-    return 0
+    //iterate through array
+    //count consecutive, keep track of biggest count 
+    //return count 
+    let max = 0
+    let curr = 0
+    arr.forEach( (num) => {
+        if(num == 1){
+            curr += 1
+        } else {
+            max = Math.max(max, curr)
+            curr = 0
+        }
+    })
+    
+    max = Math.max(max, curr)
+    
+    return max
 }
 
 exports.findMaxConsecutiveOnes = findMaxConsecutiveOnes
--- arrays/max_consecutive_ones/findMaxConsecutiveOnes.test.js
+++ arrays/max_consecutive_ones/findMaxConsecutiveOnes.test.js
@@ -6,3 +6,24 @@ test('it passes example 1', () => {
 
     expect(findMaxConsecutiveOnes(input)).toBe(output)
 })
+
+test('it passes example 2', () => {
+    let input = [1,1,1,0,1,1]
+    let output = 3
+
+    expect(findMaxConsecutiveOnes(input)).toBe(output)
+})
+
+test('it passes example 3', () => {
+    let input = [1,1,1,1,1,1]
+    let output = 6
+
+    expect(findMaxConsecutiveOnes(input)).toBe(output)
+})
+
+test('it passes example 4', () => {
+    let input = [1,1,1,1,1,0]
+    let output = 5
+
+    expect(findMaxConsecutiveOnes(input)).toBe(output)
+})
