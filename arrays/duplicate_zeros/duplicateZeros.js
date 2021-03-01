b2a1f637b12fab0d89eeafb1b8aa87fb9033c99b
JasonEb <jasonebueng@gmail.com>
2021-02-28 21:17:34 -0800

    implements concise duplicate zeros

--- arrays/duplicate_zeros/duplicateZeros.js
+++ arrays/duplicate_zeros/duplicateZeros.js
@@ -1,4 +1,22 @@
-const duplicateZeros = () => {
+const duplicateZeros = (arr) => {
+    //iterate through array
+    //if detect a zero, insert a new zero
+    //remove last element
+
+    let i = 0
+    let el = arr[i]
+
+    while (i < arr.length ) {
+        el = arr[i]
+        if (el === 0){
+            arr.splice(i, 0, 0)
+            arr.pop()
+            i += 2
+        } else {
+            i += 1
+        }
+    }
+
     return
 }
 
--- arrays/duplicate_zeros/duplicateZeros.test.js
+++ arrays/duplicate_zeros/duplicateZeros.test.js
@@ -1,50 +1,44 @@
 const {duplicateZeros} = require('./duplicateZeros')
 
-test('it passes example 1', () =>{
+test('it passes example 1', () => {
     let input = [1,0,2,3,0,4,5,0]
     let answer = [1,0,0,2,3,0,0,4]
+    duplicateZeros(input)
 
-    expect(duplicateZeros(input)).toEqual(answer)
+    expect(input).toEqual(answer)
 })
 
-test('it passes example 2', () =>{
+test('it passes example 2', () => {
     let input = [1,2,3]
     let answer = [1,2,3]
+    duplicateZeros(input)
 
-    expect(duplicateZeros(input)).toEqual(answer)
+    expect(input).toEqual(answer)
 })
 
-test('it passes example 3', () =>{
+test('it passes example 3', () => {
     let input = [0,1,2,3]
-    let answer = [0,0,1,2,3]
+    let answer = [0,0,1,2]
+    duplicateZeros(input)
 
-    expect(duplicateZeros(input)).toEqual(answer)
+    expect(input).toEqual(answer)
 })
 
-test('it passes example 3', () =>{
-    let input = [0,1,2,3]
-    let answer = [0,0,1,2,3]
-
-    expect(duplicateZeros(input)).toEqual(answer)
-})
-
-test('it passes example 4', () =>{
-    let input = [0,0,1,2,3]
-    let answer = [0,0,0,0, 1,2,3]
-
-    expect(duplicateZeros(input)).toEqual(answer)
-})
-
-test('it passes example 5', () =>{
+test('it passes example 4', () => {
     let input = [1,2,3,0]
-    let answer = [1,2,3,0,0]
+    let answer = [1,2,3,0]
+    duplicateZeros(input)
 
-    expect(duplicateZeros(input)).toEqual(answer)
+    expect(input).toEqual(answer)
 })
 
-test('it passes example 6', () =>{
-    let input = [1,2,3,0,0]
-    let answer = [1,2,3,0,0,0,0]
+test('it passes example 5', () => {
+    let input =  [8,4,5,0,0,0,0,7]
+    let answer = [8,4,5,0,0,0,0,0]
 
-    expect(duplicateZeros(input)).toEqual(answer)
+    duplicateZeros(input)
+
+    expect(input).toEqual(answer)
 })
+
+
