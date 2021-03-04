791bebe419d7e4e0957664d1ab78c48777b4eb75
JasonEb <jasonebueng@gmail.com>
2021-03-03 18:17:18 -0800

    implements removeElement

--- arrays/remove-el-in-place/removeElement.js
+++ arrays/remove-el-in-place/removeElement.js
@@ -1,5 +1,33 @@
 const removeElement = (nums, val) => {
-    return nums
+    //iterate through array
+    //once target detected, overrite the current elements
+    let curr = nums[0]
+    let i = 0;
+    let fdx = nums.length - 1
+
+    while(i <= fdx) {
+        curr = nums[i]
+
+        if (curr == val) {
+            //shift elements left
+            shiftElementsLeft(nums,i)
+            fdx--
+        } else {
+            i++
+        }
+    }
+    nums = nums.slice(0, fdx)
+    return fdx + 1
+}
+
+const shiftElementsLeft = (nums, i) => {
+    let j = ++i
+    let ogLength = nums.length
+
+    while(j <= ogLength - 1) {
+        nums[j - 1] = nums[j]
+        j++
+    }
 }
 
 exports.removeElement = removeElement
--- arrays/remove-el-in-place/removeElement.test.js
+++ arrays/remove-el-in-place/removeElement.test.js
@@ -3,15 +3,24 @@ const {removeElement} = require('./removeElement')
 test('it passes case 1', () => {
     let nums = [3,2,2,3]
     let val = 3
+    
+    expect(removeElement(nums, val)).toEqual(2)
+    expect(nums.slice(0,2)).toEqual([2,2])
 
-    expect(removeElement(nums)).toEqual(2)
-    expect(nums).toEqual([2,2])
 })
 
 test('it passes case 2', () => {
     let nums = [0,1,2,2,3,0,4,2]
     let val = 2
 
-    expect(removeElement(nums)).toEqual(5)
-    expect(nums).toEqual([0,1,4,0,3])
+    expect(removeElement(nums, val)).toEqual(5)
+    expect(nums.slice(0,5)).toEqual(expect.arrayContaining([0,1,4,0,3]))
+})
+
+test('it passes case 3', () => {
+    let nums = [2]
+    let val = 3
+
+    expect(removeElement(nums, val)).toEqual(1)
+    expect(nums.slice(0,1)).toEqual([2])
 })
