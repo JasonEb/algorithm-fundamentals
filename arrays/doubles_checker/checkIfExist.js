8cb80dfac9b57c1893538ac988bac6a734101da0
JasonEb <jasonebueng@gmail.com>
2021-03-03 20:29:03 -0800

    implements doubles checker

--- arrays/doubles_checker/checkIfExist.js
+++ arrays/doubles_checker/checkIfExist.js
@@ -1,5 +1,19 @@
 const checkIfExist = (arr) => {
-    return null
+    //double iterate through array for each combinations
+
+    let i = 0
+    let j = 1
+
+    for(i = 0; i < arr.length; i++) {
+        let a = arr[i]
+        for(j = i + 1; j< arr.length; j++) {
+            let b = arr[j]
+            if (a == 2*b || 2*a == b) {
+                return true
+            }
+        }
+    }
+    return false
 }
 
 exports.checkIfExist = checkIfExist
