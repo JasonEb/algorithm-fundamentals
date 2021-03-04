0ad8c7a7981eaa18316b18833743c23356a1cf56
JasonEb <jasonebueng@gmail.com>
2021-03-03 16:47:38 -0800

    implements merge sorted array

--- arrays/merge_sorted_arrays/merge.js
+++ arrays/merge_sorted_arrays/merge.js
@@ -1,32 +1,28 @@
 const merge = (a,m, b, n) => {
-    //maintain two pointers
-    //shuffle insert order and update pointers
-    let result = [] 
-    let adx = 0
-    let bdx = 0
+    let adx = m-1
+    let bdx = n-1
+    let cdx = m+n-1
 
-    if (b.length === 0) {
-        return a
+    //walk backwards through array
+    //fill the back with bigger element
+    while(adx >=0 && bdx>=0) {
+        if(a[adx] > b[bdx]) {
+            a[cdx] = a[adx]
+            cdx--
+            adx--
+        }
+        else {
+            a[cdx] = b[bdx]
+            cdx--
+            bdx--
+        }
     }
 
-    while(adx < m || bdx < n) {
-        //scan if elements after m 
-        if (adx > m && m > 0) {
-            a[adx] = Math.INFINITY
-        }
-
-        if (bdx > n && n > 0) {
-            b[bdx] = Math.INFINITY
-        }
-
-        if(a[adx] < b[bdx]) {
-            adx += 1
-        } else {
-            a.splice(adx, 0, b[bdx])
-            a.pop()
-            bdx += 1
-            adx += 1
-        }
+    //once adx is done, do bdx still
+    while(bdx >= 0 ) {
+        a[cdx] = b[bdx]
+        cdx--
+        bdx--
     }
 
     return a
--- arrays/merge_sorted_arrays/merge.test.js
+++ arrays/merge_sorted_arrays/merge.test.js
@@ -24,7 +24,7 @@ test('it passes example 3', () => {
     let nums1 = [1, 0]
     let m = 1
     let nums2 = [2]
-    let n = 0
+    let n = 1
     let answer = [1,2]
 
     expect(merge(nums1, m, nums2, n)).toEqual(answer)
@@ -34,8 +34,18 @@ test('it passes example 4', () => {
     let nums1 = [0]
     let m = 0
     let nums2 = [1]
-    let n = 0
+    let n = 1
     let answer = [1]
+
+    expect(merge(nums1, m, nums2, n)).toEqual(answer)
+})
+
+test('it passes example 5', () => {
+    let nums1 = [4,0,0,0,0,0]
+    let m = 1
+    let nums2 = [1,2,3,5,6]
+    let n = 5
+    let answer = [1,2,3,4,5,6]
 
     expect(merge(nums1, m, nums2, n)).toEqual(answer)
 })
