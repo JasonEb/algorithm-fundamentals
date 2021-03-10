18e9d187ef7c975572e58e7de0e34e50d3faf79c
JasonEb <jasonebueng@gmail.com>
2021-03-09 17:20:01 -0800

    implements plus one with exponent approach
    solves plusOne

--- arrays/plus_one/plusOne.js
+++ arrays/plus_one/plusOne.js
@@ -3,10 +3,26 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let newSum = parseInt(digits.join(''))+1
-    
-    return String(newSum).split('').map( (n) => parseInt(n))
+    let i = digits.length - 1
+    let carry = 1
 
+    while(carry > 0) {
+        if ( i < 0 ) {
+            digits.unshift(1)
+            break
+        }
+
+        digits[i] += 1
+        if (digits[i] > 9) {
+            carry = 1
+            digits[i] = 0
+            i--
+        } else {
+            carry = 0
+        }     
+    }
+
+    return digits
 };
 
 exports.plusOne = plusOne
--- arrays/plus_one/plusOne.test.js
+++ arrays/plus_one/plusOne.test.js
@@ -8,19 +8,47 @@ test('example 1', () => {
     let digits = [1,2,3]
     let output = [1,2,4]
 
-    expect(plusOne(digits).toEqual(output))
+    expect(plusOne(digits)).toEqual(output)
+})
+
+test('example 1a', () => {
+    let digits = [1,2,9]
+    let output = [1,3,0]
+
+    expect(plusOne(digits)).toEqual(output)
+})
+
+test('example 1b', () => {
+    let digits = [1,9,9]
+    let output = [2,0,0]
+
+    expect(plusOne(digits)).toEqual(output)
 })
 
 test('example 2', () => {
     let digits = [4,3,2,1]
     let output = [4,3,2,2]
 
-    expect(plusOne(digits).toEqual(output))
+    expect(plusOne(digits)).toEqual(output)
 })
 
 test('example 3', () => {
     let digits = [0]
     let output = [1]
 
-    expect(plusOne(digits).toEqual(output))
+    expect(plusOne(digits)).toEqual(output)
+})
+
+test('example 4', () => {
+    let digits = [9]
+    let output = [1, 0]
+
+    expect(plusOne(digits)).toEqual(output)
+})
+
+test('max integer case', () => {
+    let digits = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+    let output = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]
+
+    expect(plusOne(digits)).toEqual(output)
 })
