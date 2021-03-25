99fc395f653f6e119a4368d09860e63e90418781
JasonEb <jasonebueng@gmail.com>
2021-03-24 17:10:09 -0700

    adds crazy tree approach to kth grammar

--- recursion/kth_grammar/kthGrammar.js
+++ recursion/kth_grammar/kthGrammar.js
@@ -3,14 +3,29 @@
  * @param {number} K
  * @return {number}
  */
+
+
 var kthGrammar = function(N, K) {
+    if( N == 1) { return "0" }
+    if(K%2 == 0) {
+        let j = Math.floor(K/2)
+        if (kthGrammar(N-1, j) == "0") { return "1" }
+        else { return 0 }
+    }
+    else{
+        let j = Math.floor((K+1)/2)
+        if (kthGrammar(N-1, j) == "0" ) { return "0" }
+        else { return "1" }
+    }
+}
+
+var recursiveKthGrammar = function(N, K) {
     let row = kthRows(N-1)
 
     return row[K - 1]
 };
 
 const kthRows = (row, rows = ["0"]) => {
-    debugger
     if (row == 0) { return rows[0] }
     //grab prevrow
     //make new row and return it
