49200715e93f9eb4a2d84da7e7b0075617822514
JasonEb <jasonebueng@gmail.com>
2021-04-19 23:30:19 -0700

    adds start of cycle detection

--- linked_lists/linked_list_cycle/hasCycle.js
+++ linked_lists/linked_list_cycle/hasCycle.js
@@ -18,5 +18,27 @@ var hasCycle = function(head) {
     return true
 };
 
+var hasCycle_2 = function(head) {
+    if(head == null ) { return null }
+    let a = head
+    let b = head
+
+    while(b != null && b.next != null) {
+        b = b.next.next
+        a = a.next
+        if (a == b) { break }
+    }
+
+    if (b == null || b.next == null) { return null } 
+
+    a = head
+    while(a != b) {
+        a = a.next
+        b = b.next
+    }
+    return a
+};
+
 exports.ListNode = ListNode
 exports.hasCycle = hasCycle
+exports.hasCycle_2 = hasCycle_2
--- linked_lists/linked_list_cycle/hasCycle.test.js
+++ linked_lists/linked_list_cycle/hasCycle.test.js
@@ -1,31 +1,65 @@
-const { hasCycle, ListNode} = require('./hasCycle')
+const { hasCycle, hasCycle_2, ListNode} = require('./hasCycle')
 
-test('example 1', () => {
-    let a = new ListNode(3)
-    let b = new ListNode(2)
-    let c = new ListNode(0)
-    let d = new ListNode(-4)
+describe('has cycle 1', () => {
+    test('example 1', () => {
+        let a = new ListNode(3)
+        let b = new ListNode(2)
+        let c = new ListNode(0)
+        let d = new ListNode(-4)
 
-    a.next = b
-    b.next = c
-    c.next = d
-    d.next = b
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = b
 
-    expect(hasCycle(a)).toEqual(true)
+        expect(hasCycle(a)).toEqual(true)
+    })
+
+    test('example 2', () => {
+        let a = new ListNode(1)
+        let b = new ListNode(2)
+
+        a.next = b
+        b.next = a
+
+        expect(hasCycle(a)).toEqual(true)
+    })
+
+    test('example 3', () => {
+        let a = new ListNode(1)
+
+        expect(hasCycle(a)).toEqual(false)
+    })
 })
 
-test('example 2', () => {
-    let a = new ListNode(1)
-    let b = new ListNode(2)
+describe('has cycle 2', () => {
+    test('example 1', () => {
+        let a = new ListNode(3)
+        let b = new ListNode(2)
+        let c = new ListNode(0)
+        let d = new ListNode(-4)
 
-    a.next = b
-    b.next = a
+        a.next = b
+        b.next = c
+        c.next = d
+        d.next = b
 
-    expect(hasCycle(a)).toEqual(true)
-})
+        expect(hasCycle_2(a)).toEqual(b)
+    })
 
-test('example 3', () => {
-    let a = new ListNode(1)
+    test('example 2', () => {
+        let a = new ListNode(1)
+        let b = new ListNode(2)
 
-    expect(hasCycle(a)).toEqual(false)
+        a.next = b
+        b.next = a
+
+        expect(hasCycle_2(a)).toEqual(a)
+    })
+
+    test('example 3', () => {
+        let a = new ListNode(1)
+
+        expect(hasCycle_2(a)).toEqual(null)
+    })
 })
